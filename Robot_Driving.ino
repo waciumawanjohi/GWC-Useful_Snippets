167991e623f3cd5cdf4c89f9a0917b0d39c73421
Waciuma Wanjohi <code@waciuma.com>
2018-06-05 00:35:27 -0400

    Delete piezo references. Include a square driving path in main loop.

--- Robot_Driving.ino
+++ Robot_Driving.ino
@@ -4,33 +4,14 @@
 Servo servoLeft;                             // Declare left servo signal
 Servo servoRight;                            // Declare right servo signal
 
-int PIEZOPIN = 5;                            // Declare pin that the piezo is connected to.
-// DECLARE LED PINS HERE
-
-// One octave of notes between A4 and A5, for Piezo Greeting
-int note_A4 = 440;
-int note_As4 = 466; int note_Bb4 = note_As4;
-int note_B4 = 494;
-int note_C5 = 523;
-int note_Cs5 = 554; int note_Db5 = note_Cs5;
-int note_D5 = 587;
-int note_Ds5 = 622; int note_Eb5 = note_Ds5;
-int note_E5 = 659;
-int note_F5 = 698;
-int note_Fs5 = 740; int note_Gb5 = note_Fs5;
-int note_G5 = 784;
-int note_Gs5 = 830; int note_Ab5 = note_Gs5;
-
 void setup()
 {
-  pinMode(PIEZOPIN, OUTPUT);                 // Attach piezo to pin 5.
   servoLeft.attach(13);                      // Attach left signal to pin 13
   servoRight.attach(12);                     // Attach right signal to pin 12
-  servoLeft.writeMicroseconds(1490);        // Calibrate left servo
-  servoRight.writeMicroseconds(1490);       // Calibrate right servo
+
   // Attach LED pins here.
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);                        // Attach right blinker to pin #2
+  pinMode(3, OUTPUT);                        // Attach left blinker to pin #3
 
 }
 
@@ -41,13 +22,14 @@ void loop()
   // For help, visit https://learn.parallax.com/tutorials/robot/shield-bot/robotics-board-education-shield-arduino/chapter-2-shield-lights-servo-4.
   int speedOfCar = 200;
   int seconds = 2;
-//  forward(seconds,speedOfCar);
-//  delay(1000);
-//  backward(seconds,speedOfCar);
-//  delay(1000);
+  forward(seconds,speedOfCar);
   turnRight();
+  backward(seconds,speedOfCar);
+  turnRight();
+  forward(seconds,speedOfCar);
   turnLeft();
-  delay(6000);
+  forward(seconds,speedOfCar);
+  turnLeft();
 }
 
 void forward(int seconds, int speedOfCar){

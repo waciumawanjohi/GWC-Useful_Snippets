8a6d14e2b014bb426a46cc027126d3b0b8c8c6f5
Waciuma Wanjohi <code@waciuma.com>
2018-06-05 00:32:32 -0400

    Add functions for moving forward and backward, for turn signaling, and for left and right turns

--- Robot_Driving.ino
+++ Robot_Driving.ino
@@ -26,9 +26,11 @@ void setup()
   pinMode(PIEZOPIN, OUTPUT);                 // Attach piezo to pin 5.
   servoLeft.attach(13);                      // Attach left signal to pin 13
   servoRight.attach(12);                     // Attach right signal to pin 12
-  servoLeft.writeMicroseconds(1500);        // Calibrate left servo
-  servoRight.writeMicroseconds(1500);       // Calibrate right servo
+  servoLeft.writeMicroseconds(1490);        // Calibrate left servo
+  servoRight.writeMicroseconds(1490);       // Calibrate right servo
   // Attach LED pins here.
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
 
 }
 
@@ -37,7 +39,64 @@ void loop()
   // Code for testing servos.
   // Tinker with the numbers to see how they work!
   // For help, visit https://learn.parallax.com/tutorials/robot/shield-bot/robotics-board-education-shield-arduino/chapter-2-shield-lights-servo-4.
-  servoLeft.writeMicroseconds(1700);
-  servoRight.writeMicroseconds(1300);
-  delay(500);
+  int speedOfCar = 200;
+  int seconds = 2;
+//  forward(seconds,speedOfCar);
+//  delay(1000);
+//  backward(seconds,speedOfCar);
+//  delay(1000);
+  turnRight();
+  turnLeft();
+  delay(6000);
+}
+
+void forward(int seconds, int speedOfCar){
+  int leftSpeed = 1490+speedOfCar;
+  int rightSpeed = 1490-speedOfCar;
+  servoLeft.writeMicroseconds(leftSpeed);
+  servoRight.writeMicroseconds(rightSpeed);
+  delay(seconds*1000);
+  servoLeft.writeMicroseconds(1490); // Stop
+  servoRight.writeMicroseconds(1490);
+}
+
+void backward(int seconds, int speedOfCar){
+  forward(seconds,0-speedOfCar);
+}
+
+void turn(int speedOfCar){
+  int leftSpeed = 1490+speedOfCar;
+  int rightSpeed = 1490+speedOfCar;
+  servoLeft.writeMicroseconds(leftSpeed);
+  servoRight.writeMicroseconds(rightSpeed);
+  delay(1000);
+  servoLeft.writeMicroseconds(1490); // Stop
+  servoRight.writeMicroseconds(1490);
+}
+
+void turnRight(){
+  blinkRight(3, .5);
+  turn(60);
+}
+
+void turnLeft(){
+  blinkLeft(3, .5);
+  turn(-58);
+}
+
+void blinkRight(int times, float blinkSpeed){
+  blink(times,blinkSpeed,2);
+}
+
+void blinkLeft(int times, float blinkSpeed){
+  blink(times,blinkSpeed,3);
+}
+
+void blink(int times, float blinkSpeed, int pin){
+  for (int i = 0; i < times; i++){
+    digitalWrite(pin, HIGH);       // sets the digital pin 13 on
+    delay(blinkSpeed*1000);                  // waits for a second
+    digitalWrite(pin, LOW);        // sets the digital pin 13 off
+    delay(blinkSpeed*1000);                  // waits for a second
+  }
 }
